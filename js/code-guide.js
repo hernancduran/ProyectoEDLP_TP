ed3d2c1e5f394dd2b7477ba51a11a2909e6166d6
hernancduran <104223695+hernancduran@users.noreply.github.com>
2022-06-07 15:30:15 -0300

    Add files via upload

---
+++ js/code-guide.js
@@ -0,0 +1,5 @@
+function contactame() { alert ("221 556-4160 - hernancduran@gmail.com")}
+
+function fondooscuro() { document.body.style.backgroundColor = "black"; }
+
+function fondoclaro() { document.body.style.backgroundColor = "white" ; }
